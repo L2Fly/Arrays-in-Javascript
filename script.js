8570aa11f40ced0c112475f488f639d1ee5496e9
Laura Weyrick <65510690+L2Fly@users.noreply.github.com>
2020-05-31 22:44:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var myArray = ["sanic", "enchiladas", "fisheroos", "dataman",];
+
+var random =prompt("What is your favorite random word?");
+
+for(var i=0; i<myArray.length; i++){
+  document.write(myArray[i] + random)
+}
+ for(var i=0; i<myArray.length; i++){ 
+  document.write(random + myArray[i])
+}
+ for(var i=1; i<myArray.length; i++){
+   document.write(myArray[2] + myArray[4])
+ }
+for(var i=0; i<myArray.length; i++){
+  document.write(myArray[])
+}
+for(var i=0; i<myArray.length; i++){
+  document.write(myArray[alphabetical])
+}
